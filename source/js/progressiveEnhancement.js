738b6e3bbbb7c47fee5e7b31124df8d6c5812701
Scott Jarvis <scott.jarvis@bbc.co.uk>
2016-04-01 12:28:47 +0100

    fixed the progressve enhancement stuff

--- source/js/progressiveEnhancement.js
+++ source/js/progressiveEnhancement.js
@@ -4,19 +4,7 @@ define(['wrapper', 'jquery'], function (wrapper, $) {
 
     ProgressiveEnhancement.prototype = {
         init: function () {
-            $( '.bbc-news-vj-video-wrapper--hero' ).css( 'display', 'inherit' );
-            var self = this;
-            self.resizer();
-            $(window).resize(function(){
-                self.resizer();
-            });
-        },
-        resizer: function () {
-            if (window.innerWidth > 1008){
-                $( '.bbc-news-vj-wrapper ' ).css( 'padding-top', '60%' );
-            } else {
-                $( '.bbc-news-vj-wrapper ' ).css( 'padding-top', '0' );
-            }
+            $( '.bbc-news-vj-wrapper' ).addClass('bbc-news-vj-wrapper--js-enabled');
         }
     };
 
