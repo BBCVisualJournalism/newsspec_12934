8eda902d961f8f72194237274b3abd4767153b48
Punit Shah <punit.shah@bbc.co.uk>
2016-04-05 11:51:59 +0100

    0.2.2

--- source/js/app.js
+++ source/js/app.js
@@ -4,19 +4,21 @@ define(['wrapper', 'video', 'sharetools', 'istatsLogger', 'progressiveEnhancemen
 
     var hero_holding_img;
 
+    var version = '0.2.2';
+
     if (window.innerWidth > 1008){
         // desktop holding img
-        hero_holding_img = 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/976xcecesinkde27.jpg?v=0.2.1';
+        hero_holding_img = 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/976xcecesinkde27.jpg?v=' + version;
     } else {
         // mobile holding img
-        hero_holding_img = 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/976xsenade47.jpg?v=0.2.1';
+        hero_holding_img = 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/976xsenade47.jpg?v=' + version;
     }
     new Video('#bbc-news-vj-video--hero',  'p03pzhl3', hero_holding_img, true);
-    new Video('#bbc-news-vj-video--one',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/mami-dog-cropped.jpg?v=0.2.1', false);
-    new Video('#bbc-news-vj-video--two',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/miriamde22.jpg?v=0.2.1', false);
-    new Video('#bbc-news-vj-video--three', 'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/daniele.jpg?v=0.2.1', false);
-    new Video('#bbc-news-vj-video--four',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/cece_smile.jpg?v=0.2.1', false);
-    new Video('#bbc-news-vj-video--five',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/sharon.jpg?v=0.2.1', false);
+    new Video('#bbc-news-vj-video--one',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/mami-dog-cropped.jpg?v=' + version, false);
+    new Video('#bbc-news-vj-video--two',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/miriamde22.jpg?v=' + version, false);
+    new Video('#bbc-news-vj-video--three', 'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/daniele.jpg?v=' + version, false);
+    new Video('#bbc-news-vj-video--four',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/cece_smile.jpg?v=' + version, false);
+    new Video('#bbc-news-vj-video--five',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/sharon.jpg?v=' + version, false);
 
     new ShareTools('#bbc-news-vj-sharetools--1');
     new ShareTools('#bbc-news-vj-sharetools--2');
