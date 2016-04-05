fbf5aa37d52f9e705098cad56acc6d00dac74882
Punit Shah <punit.shah@bbc.co.uk>
2016-04-05 11:22:42 +0100

    replaced section 4 poster image
    also refactored sections.scss

--- source/js/app.js
+++ source/js/app.js
@@ -15,7 +15,7 @@ define(['wrapper', 'video', 'sharetools', 'istatsLogger', 'progressiveEnhancemen
     new Video('#bbc-news-vj-video--one',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/mami-dog-cropped.jpg?v=0.2.1', false);
     new Video('#bbc-news-vj-video--two',   'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/miriamde22.jpg?v=0.2.1', false);
     new Video('#bbc-news-vj-video--three', 'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/daniele.jpg?v=0.2.1', false);
-    new Video('#bbc-news-vj-video--four',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/cecedreamy27.jpg?v=0.2.1', false);
+    new Video('#bbc-news-vj-video--four',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/cece_smile.jpg?v=0.2.1', false);
     new Video('#bbc-news-vj-video--five',  'p03912vm', 'http://newsimg.bbc.co.uk/news/special/2016/newsspec_12934/content/full-width/common/img/sharon.jpg?v=0.2.1', false);
 
     new ShareTools('#bbc-news-vj-sharetools--1');
